e8ebbc22c6111b09f2c86909cf287c80860594a3
Hermes Belarmino Barbosa <hermes-bb@hotmail.com>
2022-05-10 21:16:34 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-// import {ReactComponent as GithubIcon} from './assets/img/github.svg';
-// import logo from './logo.svg';
-// import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
